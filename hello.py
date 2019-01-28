822c1cf08cee0d539f796ff73e982ea9bd8f7ab4
Sulistiyanto <sulistiyanto404@gmail.com>
2019-01-28 08:34:02 +0700

    first commit

---
+++ hello.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Vector
+x = np.array((1,2,3))
+print(x)
+print("Vector Dimensions: {}".format(x.shape))
+print("Vector Size: {}".format(x.size))
+
+# Matrics
+y = np.matrix([[1,2,3], [4,5,6], [7,8,9]])
+print(y)
+print("Matrics Dimensions: {}".format(y.shape))
+print("Matrics Size: {}".format(y.size))
